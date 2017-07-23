ccc0f5607def62483c14cc6da149eb5941b7a1c2
Pravesh Chapagain <spravesh1818@gmail.com>
2017-07-23 19:23:55 +0545

    gitignore may work

--- readme.md
+++
@@ -1,6 +0,0 @@
-After copying run the commands in terminal
-->composer update
-Then
-->php artisan migrate
-
-Then you are good to go 
--- resources/views/single.blade.php
+++
@@ -1,7 +0,0 @@
-
-@include('partials._head')
-@include('partials._header')	
-@include('partials._single')
-@include('partials.rightbarSingle')
-@include('partials._recent')
-@include('partials._footer')					
