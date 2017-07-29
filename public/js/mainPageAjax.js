46cdb31dd27a71e3e3f5c63789a25fd3c2cfeea6
Pravesh Chapagain <spravesh1818@gmail.com>
2017-07-29 07:11:27 +0545

    small change

--- public/js/mainPageAjax.js
+++ public/js/mainPageAjax.js
@@ -7,7 +7,7 @@ $(document).ready(function (){
 		success:function(data){
 			var response=JSON.parse(data);
 			for(i=0;i<response.length;i++)
-			 $('#output').append('<li><a href="http://127.0.0.1:8000/categories/'+response[i].id+'">'+response[i].title+'</a></li>')
+			 $('#output').append('<li><a href="/categories/'+response[i].id+'">'+response[i].title+'</a></li>')
 		},
 		error:function(){
 			alert('error saving order');
