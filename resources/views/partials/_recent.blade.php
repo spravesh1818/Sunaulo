67c86b99791e19da362d27f4cc7356a292337fc2
shoesheill <shoesheill@gmail.com>
2017-07-30 14:26:06 +0545

    Add files via upload

---
+++ resources/views/partials/_recent.blade.php
@@ -0,0 +1,10 @@
+<div>				<!--<h4 class="box_header page_margin_top_section">Tags</h4>
+				<ul class="taxonomies clearfix page_margin_top">
+					<li>
+						<a href="#" title="Business">BUSINESS</a>
+					</li>-->
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
