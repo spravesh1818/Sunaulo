87018074fe718388ec7fa72dd172b33f0307b0da
karkiswapnil <30380026+karkiswapnil@users.noreply.github.com>
2017-07-31 12:53:45 +0545

    Add files via upload

--- resources/views/partials/highlights.blade.php
+++ resources/views/partials/highlights.blade.php
@@ -4,11 +4,20 @@
   <img src={{ asset('uploads/'.$articles[0]->image) }} style="width:100%;text-align: 'center';">
   @endif
   <br>
-  <h1 class="with_number box_header">
-	<a href="{{ route('view',$articles[0]->id) }}" title="{{$articles[0]->title}}">{{$articles[0]->title}}</a> </h1>                	
+  <h2 class="box_header_title">
+	<a href="{{ route('view',$articles[0]->id) }}" title="{{$articles[0]->title}}">{{$articles[0]->title}}</a> </h2>      
+
+  
+  <h2 class="box_header_title">
+  <a href="{{ route('view',$articles[0]->id) }}" title="{{$articles[0]->title}}">{{$articles[1]->title}}</a> </h2>  
+
+  
+  <h2 class="box_header_title">
+  <a href="{{ route('view',$articles[0]->id) }}" title="{{$articles[0]->title}}">{{$articles[3]->title}}</a> </h2>  
+
 </div>
 
-<div class="col-3 right" style="padding-top: 25px;">
+<div class="col-3 right" style="padding-top: 10px;">
  <div>
     <h4 class="box_header"> खुल्दुली </h4>
     <ul id="special">
