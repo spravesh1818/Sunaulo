e7943865bbe411c05ff48589976339faba8d1279
shoesheill <shoesheill@gmail.com>
2017-08-01 14:19:47 +0545

    Update highlights.blade.php

--- resources/views/partials/highlights.blade.php
+++ resources/views/partials/highlights.blade.php
@@ -1,7 +1,13 @@
 
 <div class="col-8" >
   @if(!empty($articles[0]->image))
+  <div style="width:60%;">
   <img src={{ asset('uploads/'.$articles[0]->image) }} style="width:100%;text-align: 'center';">
+  </div>
+  <div style="width:40%;>
+  <h2 class="box_header_title">
+	<a href="{{ route('view',$articles[0]->id) }}" title="{{$articles[0]->title}}">{{$articles[0]->title}}</a> </h2>  
+	</div>
   @endif
   <br>
   <h2 class="box_header_title">
