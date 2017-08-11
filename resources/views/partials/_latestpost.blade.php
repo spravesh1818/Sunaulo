7b6190d81ee84b1b27d7aa902b2992a6572a45f6
Pravesh Chapagain <spravesh1818@gmail.com>
2017-08-11 12:32:06 +0545

    Limiting Post

--- resources/views/partials/_latestpost.blade.php
+++ resources/views/partials/_latestpost.blade.php
@@ -51,7 +51,7 @@
 					@endphp
 <!-- First Latest Post-->@foreach($articles as $article)
 						@if($j<2)
-						@if($article->id%2==0)
+						@if($article->id%2==1)
 <!-- First Latest Post--><li class="post">
 							@if(!empty($article->image))
 							<a href="{{ route('view',$article->id) }}" title={{$article->title}}>
