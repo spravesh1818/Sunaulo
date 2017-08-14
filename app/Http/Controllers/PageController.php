df355aefbe10c196c13f258e4330daab0cd308d8
Pravesh Chapagain <spravesh1818@gmail.com>
2017-08-14 17:42:24 +0545

    Comments fixed

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -27,7 +27,7 @@ class PageController extends Controller
 	public function show($id){
 		$article=articles::find($id);
 		$user=User::all()->where('name',$article->author);
-		dd($author);
+		dd($user);
 		/*foreach($user as $user)
 			$author=$user;
 			echo $author;
