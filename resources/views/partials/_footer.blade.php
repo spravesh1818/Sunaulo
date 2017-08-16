ba67758526571317b21241550952980f62194fbd
Swapnil <swapnilk21030@gmail.com>
2017-08-16 15:22:43 +0545

    link in footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,14 +1,14 @@
 <div class="w3-row">
 <div id="footer">
 <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="icon-chevron-up"></i></button>
-  <div class="w3-col l7 m6 s4 w3-dark-grey w3-center"><img class="row-3" src="{{ URL::asset('images/plan.png')}}"/></div>
-  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center"><img class="row-3" src="{{ URL::asset('images/solid.png')}}"/></div>
-  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center"><img class="row-3" src="{{ URL::asset('images/girls.png')}}"/></div>
+  <div class="w3-col l7 m6 s4 w3-dark-grey w3-center"><a href="https://plan-international.org/nepal"><img class="row-3" src="{{ URL::asset('images/plan.png')}}"/></a></div>
+  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center"><a href="http://solidnepal.org.np"><img class="row-3" src="{{ URL::asset('images/solid.png')}}"/></a></div>
+  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center"><a href="http://www.girlsnotbridesnepal.org.np"><img class="row-3" src="{{ URL::asset('images/girls.png')}}"/></a></div>
 
 
-  <div class="w3-col l7 m6 s4 w3-dark-grey w3-center">@include('partials._socialicon')</div>
-  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">@include('partials._socialicon')</div>
-  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">@include('partials._socialicon')</div>
+  <div class="w3-col l7 m6 s4 w3-dark-grey w3-center">@include('partials._plansocialicon')</div>
+  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">@include('partials._solidsocialicon')</div>
+  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">@include('partials._gnbsocialicon')</div>
   <br><br><br> 
 
 <div class="w3-col l7 m6 s4 w3-dark-grey w3-center">© Copyright</div>
