d3797e15836c2edd7af38dfff26bf748ded78eab
Pravesh Chapagain <spravesh1818@gmail.com>
2017-08-19 10:43:10 +0545

    Category editing+quiz bug solved

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -164,9 +164,9 @@ class PageController extends Controller
 			$sel=(string)$j;
 			$choice="choice".$sel;
 			$answer="answer".$sel;
-			echo $request->choice;
+			echo $request->$choice;
 			echo "<br>";
-			echo $request->answer;
+			echo $request->$answer;
 			echo "<br>";
 			if($request->$choice===$request->$answer){
 				$points++;
