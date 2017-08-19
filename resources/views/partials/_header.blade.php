62af9851903dadae080499bdc99653cf0884b37a
Swapnil <swapnilk21030@gmail.com>
2017-08-19 10:33:42 +0545

    message

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -11,7 +11,9 @@
 				<div class="header clearfix" >
 					<div class="logo">
 					<br>
-						<div><h1 style="text-align: center; color:#F62459;">सहि उमेर						<span style="color:#00B16A;">सहि कदम</span></h1>
+						<div><h1 style="text-align: center; color:#F62459;">सहि <span style="color:#00B16A;">उमेर </span>
+						<span style="color:#F62459;">सहि </span>
+						<span style="color:#00B16A;">कदम </span></h1>
 						</div>
 						</div>
 					
