2f1a2fdc4d8eded44661bc0bc13e7f4fb39b134d
Swapnil <swapnilk21030@gmail.com>
2017-08-19 10:51:13 +0545

    header

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -14,7 +14,7 @@
 						<div><h1 style="text-align: center; color:#F62459;">सहि <span style="color:#3A539B;">उमेर </span>
 						<span style="color:#F62459;">सहि </span>
 						<span style="color:#3A539B;">कदम </span></h1>
-						<h2 style="text-align: center; color:#CF000F;>right age right decision</h2>
+						<h2 style="text-align: center;"> <span style=" color:#CF000F;">right age right decision</span></h2>
 						</div>
 						</div>
 					
