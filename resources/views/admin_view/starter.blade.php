6f8155bd7ea2cd86836dddaf7969e0e3b3bc592e
Pravesh Chapagain <spravesh1818@gmail.com>
2017-09-15 10:37:49 +0545

    Minor changes and fixes

--- resources/views/admin_view/starter.blade.php
+++ resources/views/admin_view/starter.blade.php
@@ -1,65 +1,26 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Admin Panel|@yield('title')</title>
-  <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.6 -->
-  <!-- Font Awesome -->
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
 
   <link rel="stylesheet" href={{asset('css/preeti.css')}}>
   <link rel="stylesheet" href={{asset('css/style1.css')}}>
   @yield('stylesheets')
 
-  <!-- Ionicons -->
+  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-        page. However, you can choose any other skin. Make sure you
-        apply the skin class to the body tag so the changes take effect.
-  -->
-    <link rel="stylesheet" href={{ URL::asset('css/AdminLTE.min.css')}}>
-    <link rel="stylesheet" href={{ URL::asset('css/bootstrap.min.css') }}>
-    <link rel="stylesheet" href={{ URL::asset('css/skin-red.min.css')}}>
-    <link rel="stylesheet" href={{ URL::asset('css/myFont.css')}}>
-    
   
-  
+    <link rel="stylesheet" href={{URL::asset('css/AdminLTE.min.css')}}>
+    <link rel="stylesheet" href={{URL::asset('css/bootstrap.min.css') }}>
+    <link rel="stylesheet" href={{URL::asset('css/skin-red.min.css')}}>
+    <link rel="stylesheet" href={{URL::asset('css/myFont.css')}}>
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 </head>
-<!--
-BODY TAG OPTIONS:
-=================
-Apply one or more of the following classes to get the
-desired effect
-|---------------------------------------------------------|
-| SKINS         | skin-blue                               |
-|               | skin-black                              |
-|               | skin-purple                             |
-|               | skin-yellow                             |
-|               | skin-red                                |
-|               | skin-green                              |
-|---------------------------------------------------------|
-|LAYOUT OPTIONS | fixed                                   |
-|               | layout-boxed                            |
-|               | layout-top-nav                          |
-|               | sidebar-collapse                        |
-|               | sidebar-mini                            |
-|---------------------------------------------------------|
--->
 <body class="hold-transition skin-red sidebar-mini">
 <div class="wrapper">
 
@@ -88,69 +49,53 @@ desired effect
             <!-- Menu toggle button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-envelope-o"></i>
-              <?php #if inbox=0 then don't show the span below
-              ?>
               <span class="label label-success">{{0}}</span>
             </a>
             <ul class="dropdown-menu">
-              <li class="header"><!--Your messages-->Here goes the message Header</li>
+              <li class="header">Here goes the message Header</li>
               <li>
-                <!-- inner menu: contains the messages -->
                 <ul class="menu">
-                  <li><!-- start message -->
+                  <li>
                     <a href="#">
                       <div class="pull-left">
-                        <!-- User Image -->
                         <img src={{URL::asset('images/user2-160x160.jpg')}} class="img-circle" alt="User Image">
                       </div>
-                      <!-- Message title and timestamp -->
                       <h4>
                         Message title
                         <small><i class="fa fa-clock-o"></i> Time of arrival</small>
                       </h4>
-                      <!-- The message -->
                       <p>Message Body</p>
                     </a>
                   </li>
-                  <!-- end message -->
                 </ul>
-                <!-- /.menu -->
               </li>
               <li class="footer"><a href="#">See All Messages</a></li>
             </ul>
           </li>
-          <!-- Tasks Menu -->
           
-          <!-- User Account Menu -->
           <li class="dropdown user user-menu">
-            <!-- Menu Toggle Button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <!-- The user image in the navbar-->
               @if(!empty(auth()->user()->usr_avatar))
                 <img src={{URL::asset('uploads/'.auth()->user()->usr_avatar)}} class="user-image" alt="User Image">
              @else
               <img src={{URL::asset('images/user2-160x160.jpg')}} class="user-image" alt="User Image">
               @endif
-              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              
               <span class="hidden-xs">{{auth()->user()->name}}</span>
             </a>
             <ul class="dropdown-menu">
-              <!-- The user image in the menu -->
               <li class="user-header">
                 @if(!empty(auth()->user()->usr_avatar))
                 <img src={{URL::asset('uploads/'.auth()->user()->usr_avatar)}} class="img-circle" alt="User Image">
                 @else
                <img src={{URL::asset('images/user2-160x160.jpg')}} class="img-circle" alt="User Image">
               @endif
-
                 <p>
                   {{auth()->user()->name}}
                   <small>Designation</small>
                 </p>
               </li>
-              <!-- Menu Body -->
               
-              <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
                   <a href="#" class="btn btn-default btn-flat">Profile</a>
@@ -161,19 +106,16 @@ desired effect
               </li>
             </ul>
           </li>
-          <!-- Control Sidebar Toggle Button -->
           
         </ul>
       </div>
     </nav>
   </header>
-  <!-- Left side column. contains the logo and sidebar -->
+  
   <aside class="main-sidebar">
 
-    <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
 
-      <!-- Sidebar user panel (optional) -->
       <div class="user-panel">
         <div class="pull-left image">
         @if(!empty(auth()->user()->usr_avatar))
@@ -185,15 +127,11 @@ desired effect
         </div>
         <div class="pull-left info">
           <p>{{auth()->user()->name}}</p>
-          <!-- Status -->
           
         </div>
       </div>
-    <!-- /.search form -->
-      <!-- Sidebar Menu -->
       <ul class="sidebar-menu">
         <li class="header">Menu Items</li>
-        <!-- Optionally, you can add icons to the links -->
         <li><a href="/"><i class="fa fa-home"></i> <span>DashBoard</span></a></li>
         
         <li class="treeview">
@@ -318,16 +256,12 @@ desired effect
             </span>
           </a>
         </li>
-        
       </ul>
-      <!-- /.sidebar-menu -->
     </section>
-    <!-- /.sidebar -->
   </aside>
 
-<!-- Content Wrapper. Contains page content -->
+
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
         @yield('main_header')
@@ -341,27 +275,19 @@ desired effect
       </ol>
     </section>
 
-    <!-- Main content -->
     @yield('content')
-    <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
 
-  <!-- Main Footer -->
   <footer class="main-footer">
-    <!-- To the right -->
     <div class="pull-right hidden-xs">
       
     </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2017 <a href="www.webcreations.com">WebCreations Inc</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2017 <a href="">WebCreations Inc</a>.</strong> All rights reserved.
   </footer>
 
-<!-- Control Sidebar -->
   
   
 </div>
-<!-- ./wrapper -->
 
 <!-- REQUIRED JS SCRIPTS -->
 
