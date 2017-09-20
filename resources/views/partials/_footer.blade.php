482562ce822a9f1bcf756b65ba02aeeb8dd71e13
karkiswapnil <30380026+karkiswapnil@users.noreply.github.com>
2017-09-20 18:08:51 +0545

    changesinheaderfooter

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,18 +1,18 @@
 <div class="w3-row">
 <div id="footer">
 <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="icon-chevron-up"></i></button>
+
   <div class="w3-col l7 m6 s4 w3-dark-grey w3-center"><a href="https://plan-international.org/nepal"><img class="row-3" src="{{ URL::asset('images/plan.png')}}"/></a></div>
   <div class="w3-col l7 m3 s4 w3-dark-grey w3-center"><a href="http://solidnepal.org.np"><img class="row-3" src="{{ URL::asset('images/solid.png')}}"/></a></div>
   <div class="w3-col l7 m3 s4 w3-dark-grey w3-center"><a href="http://www.girlsnotbridesnepal.org.np"><img class="row-3" src="{{ URL::asset('images/girls.png')}}"/></a></div>
-
   <div class="w3-col l7 m6 s4 w3-dark-grey w3-center">@include('partials._plansocialicon')</div>
   <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">@include('partials._solidsocialicon')</div>
   <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">@include('partials._gnbsocialicon')</div>
   <br><br><br><br><br>
 
 <div class="w3-col l7 m6 s4 w3-dark-grey w3-center">© Copyright</div>
-  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">Developed By</div>
-  <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">About us</div>
+ <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">Developed By</div>
+ <div class="w3-col l7 m3 s4 w3-dark-grey w3-center">About us</div>
 </div>
 </div>
 </div>
