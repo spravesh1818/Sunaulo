de03f40c09869ef32c086e5ee2f09c7d31de7f27
karkiswapnil <30380026+karkiswapnil@users.noreply.github.com>
2017-09-29 12:05:07 +0545

    aboutus

--- resources/views/partials/rightbarSingle.blade.php
+++ resources/views/partials/rightbarSingle.blade.php
@@ -31,7 +31,7 @@
 							</li>
 							@endforeach
 						</ul>
-						<a class="more page_margin_top" style="font-size: 16px" href="#">थप प्रस्तुती</a>
+						<a class="more page_margin_top" style="font-size: 16px" href="{{ route('sortMR') }}">थप प्रस्तुती</a>
 					</div>
 					<div id="sidebar-most-commented">
 						<ul class="blog rating page_margin_top clearfix">
@@ -50,6 +50,6 @@
 							</li>
 							@endforeach
 						</ul>
-						<a class="more page_margin_top" style="font-size: 16px" href="#">थप प्रस्तुती</a>
+						<a class="more page_margin_top" style="font-size: 16px" href="{{ route('sortNC') }}">थप प्रस्तुती</a>
 					</div>
 				</div>
