4ba9befe9ca4d5532d099e6610521dcd64e0cfba
karkiswapnil <30380026+karkiswapnil@users.noreply.github.com>
2017-10-06 10:22:16 +0545

    scriptremoveofsurveymonkey

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,9 +13,6 @@
 
 
 
-<script>(function(t,e,s,o){var n,a,c;t.SMCX=t.SMCX||[],e.getElementById(o)||(n=e.getElementsByTagName(s),a=n[n.length-1],c=e.createElement(s),c.type="text/javascript",c.async=!0,c.id=o,c.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd9GwkiV6ZRtn4o2YIej3Tcgvg9Tynil0xh6Q1R1lgnWE.js"].join(""),a.parentNode.insertBefore(c,a))})(window,document,"script","smcx-sdk");</script>
-
-
 <!-- Latest Post From One Category-->
 <script type="text/javascript">
 	$(document).ready(function (){
