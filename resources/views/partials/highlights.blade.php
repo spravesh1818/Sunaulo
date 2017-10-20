45e37e47dd62b7079494e129cfff8384f78ec635
karkiswapnil <30380026+karkiswapnil@users.noreply.github.com>
2017-10-20 19:03:24 +0545

    imageinheadlights

--- resources/views/partials/highlights.blade.php
+++ resources/views/partials/highlights.blade.php
@@ -1,4 +1,3 @@
-
 <div class="col-8" >
   @php
     $i=0;
@@ -8,7 +7,15 @@
   @else
   @if($i<=2) 
   <h2 class="box_header_title">
-  @if(!empty($article->video_url))
+ 
+  @if(!empty($article->image))
+            <a href="{{ route('view',$article->id) }}" title={{$article->title}}>
+<!-- Image of latest Post--><img style="pointer-events: none; cursor: default; width: 100%;
+    height: auto;" src='{{ asset('uploads/'.$article->image) }}' alt='img'>
+              </a>
+              <br>
+
+  @elseif(!empty($article->video_url))
   @php
     $url = $article->video_url;
       preg_match(
