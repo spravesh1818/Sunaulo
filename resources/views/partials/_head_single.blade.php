66f189e0189799b704785000daec1ba1c707c0ec
Pravesh Chapagain <spravesh1818@gmail.com>
2017-12-28 19:29:32 +0545

    Og meta data added

--- resources/views/partials/_head_single.blade.php
+++ resources/views/partials/_head_single.blade.php
@@ -17,9 +17,10 @@
 		
   <meta property="og:url"           content="http://sahiumersahikadam.org" />
   <meta property="og:type"          content="website" />
-  <meta property="og:title"         content="" />
-  <meta property="og:description"   content="" />
+  <meta property="og:title"         content="{{$article->title}}" />
+  <meta property="og:description"   content='{!!substr($article->content,0,300)!!}' />
   <meta property="og:image"         content="" />
+ <div height="500px" width="500px">{{$article->title}}</div>
 
 
 		<!--style-->
